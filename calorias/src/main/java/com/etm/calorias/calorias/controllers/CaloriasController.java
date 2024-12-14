9a85e6667b3313736d185ec830987eabad82e210
ewertontm <ewerton.startup@gmail.com>
2024-12-14 08:31:23 -0300

    implementações - continuacao

--- calorias/src/main/java/com/etm/calorias/calorias/controllers/CaloriasController.java
+++ calorias/src/main/java/com/etm/calorias/calorias/controllers/CaloriasController.java
@@ -6,18 +6,41 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/calorias")
+@RequestMapping("/api/v1")
+
 public class CaloriasController {
 
+	@RequestMapping("/calorias")
 	@GetMapping(produces = "application/json")
     public ResponseEntity<List<String>> findAll(){
      List<String> listaCalorias = new ArrayList<String>();
      listaCalorias.add("Maça - 100 gramas - 52 calorias");
      
+     
+     return new ResponseEntity<>(listaCalorias, HttpStatus.OK);
+    }
+	
+	
+	@RequestMapping("/calorias/{nomeAlimento}")
+	@GetMapping(produces = "application/json")
+    public ResponseEntity<List<String>> findAlimento(@PathVariable String nomeAlimento){
+    List<String> listaCalorias = new ArrayList<String>();
+     
+     
+     if (nomeAlimento.equals("maça")) {
+    	 listaCalorias.add("Maça - 100 gramas - 52 calorias");
+     } else if (nomeAlimento.equals("pera")) {
+    	 listaCalorias.add("Pera - 100 gramas - 57 calorias");
+     } else if (nomeAlimento.equals("coco")) {
+    	 listaCalorias.add("Coco - 100 gramas - 354 calorias");
+     } else {
+    	 listaCalorias.add(nomeAlimento + " não localizado");
+     }
      
      return new ResponseEntity<>(listaCalorias, HttpStatus.OK);
     }
